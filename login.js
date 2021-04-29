e7c011fa41e4e3367aef3cfa02b325fa2a7d5bcd
Vayle <51290880+Vaylentine@users.noreply.github.com>
2021-04-29 02:40:47 +0200

    lol

--- login.js
+++ login.js
@@ -1,3 +1,10 @@
+function LoadingScreen()
+{
+   document.querySelector("#before_preloader").style.display = "none"
+}
+
+setTimeout(function(){ LoadingScreen(); }, 2000);
+
 function passShow()
 {
 
@@ -15,3 +22,30 @@ let passVis = document.querySelector("#input-73")
 passVis.type = "password"
 document.querySelector(".g-input__show-password").setAttribute('onclick','passShow()')
 }
+
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    
+    direction: 'horizontal',
+    loop: true,
+  
+    autoplay: {
+        delay: 4000,
+      },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  
